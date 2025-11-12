57179a8673955b8c14d4bacdcfeb1ed2ee1133d0
VinayMandhaan <v.kumar@sellanycar.com>
2025-11-12 19:03:18 +0500

    Exception Handler for HTTP

--- qashio-api/src/main.ts
+++ qashio-api/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors()
   const config = new DocumentBuilder()
     .setTitle('Expense Tracker API')
